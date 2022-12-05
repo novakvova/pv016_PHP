777bd93260bd567c9fc52b8b9f105e81b4af553a
novakvova <novakvova@gmail>
2022-12-05 18:59:51 +0200

    Begin php

---
+++ register.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Головна сторінка</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<?php include "_header.php" ?>
+
+<div class="container">
+    <h1 class="text-center">Реєстрація</h1>
+</div>
+
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
