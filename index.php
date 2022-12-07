2fdda61fb91961ab4b520c2db03eeb741e716a48
novakvova <novakvova@gmail>
2022-12-07 19:09:36 +0200

    Working show list products

--- index.php
+++ index.php
@@ -13,9 +13,60 @@
 
 <?php include "_header.php" ?>
 
+<?php
+try {
+    $user="root";
+    $pass="";
+    $dbh = new PDO('mysql:host=localhost;dbname=pv016', $user, $pass);
+    ?>
+
+
+
 <div class="container">
     <h1 class="text-center">Список продуктів</h1>
+    <section style="background-color: #eee;">
+        <div class="container py-5">
+            <div class="row">
+                <?php
+                foreach($dbh->query('SELECT * FROM tbl_products') as $row) {
+                    $image = $row["image"];
+                    $name = $row['name'];
+                    $price = $row['price'];
+                    echo'
+                <div class="col-md-6 col-lg-4 mb-4 mb-md-0">
+                    <div class="card">
+                        <img src="/images/'.$image.'"
+                             class="card-img-top" alt="Gaming Laptop"/>
+                        <div class="card-body">
+
+                            <div class="d-flex justify-content-between mb-3">
+                                <h5 class="mb-0">'.$name.'</h5>
+                                <h5 class="text-dark mb-0">$'.$price.'</h5>
+                            </div>
+
+                            <div class="mb-2 text-end">
+                                <button type="button" class="btn btn-success">Купить</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                    ';
+                }
+                ?>
+
+
+            </div>
+        </div>
+    </section>
 </div>
+
+    <?php
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
 
 <script src="js/bootstrap.bundle.min.js"></script>
 </body>
