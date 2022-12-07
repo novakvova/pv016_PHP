93276e689c251171456fe3762cf6708eafeba3aa
novakvova <novakvova@gmail>
2022-12-07 18:15:21 +0200

    Add register begin select image

--- register.php
+++ register.php
@@ -15,6 +15,29 @@
 
 <div class="container">
     <h1 class="text-center">Реєстрація</h1>
+
+    <form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Пошта</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label for="selectFile" class="form-label">
+                <span>Фото</span>
+                <img src="/images/select.jpg" width="300"
+                     alt="select" style="cursor: pointer">
+            </label>
+            <input type="file"
+                   class="form-control d-none"
+                   id="selectFile"
+                   name="selectFile">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Пароль</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 </div>
 
 <script src="js/bootstrap.bundle.min.js"></script>
