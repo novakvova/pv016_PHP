842be2686c9614495c250098dba53f49eaedfb2e
novakvova <novakvova@gmail>
2022-12-16 19:06:37 +0200

    Begin working add product

--- add_product.php
+++ add_product.php
@@ -4,27 +4,44 @@ if($_SERVER["REQUEST_METHOD"]=="POST") {
     $price = $_POST['price'];
     $description = $_POST['description'];
     //print_r([$name, $price, $description]);
-    include($_SERVER['DOCUMENT_ROOT'].'/lib/guidv4.php');
-    $image_name = guidv4().'.jpeg';
-    $dir_save="images/";
-    $uploadfile = $dir_save.$image_name;
-    if(move_uploaded_file($_FILES['image']["tmp_name"], $uploadfile))
-    {
+
+
         include($_SERVER['DOCUMENT_ROOT'].'/options/connection_database.php');
-        $sql = "INSERT INTO `tbl_products` (`name`, `image`, `price`, `datecrate`, `description`) VALUES (:name, :image, :price, NOW(), :description);";
+        $sql = "INSERT INTO `tbl_products` (`name`, `price`, `datecrate`, `description`) VALUES (:name, :price, NOW(), :description);";
         $stmt = $dbh->prepare($sql);
         $stmt->bindParam(':name', $name);
         $stmt->bindParam(':price', $price);
         $stmt->bindParam(':description', $description);
-        $stmt->bindParam(':image', $image_name);
         $stmt->execute();
+
+    include($_SERVER['DOCUMENT_ROOT'].'/lib/guidv4.php');
+    $sql = "SELECT LAST_INSERT_ID() as id;";
+    $item = $dbh->query($sql)->fetch();
+    $insert_id = $item['id'];
+
+    $images = $_POST['images'];
+    $count=1;
+    foreach ($images as $base64) {
+        $dir_save = 'images/';
+        $image_name = guidv4() . '.jpeg';
+        $uploadfile = $dir_save . $image_name;
+        list(, $data) = explode(',', $base64);
+        $data = base64_decode($data);
+        file_put_contents($uploadfile, $data);
+        $sql = 'INSERT INTO tbl_product_images (name, datecreate, priority, product_id) VALUES(:name, NOW(), :priority, :product_id);';
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindParam(':name', $image_name);
+        $stmt->bindParam(':priority', $count);
+        $stmt->bindParam(':product_id', $insert_id);
+        $stmt->execute();
+        $count++;
+    }
+
+
+
         header("Location: /");
         exit();
-    }
-    else {
-        echo "Error save file";
-        exit();
-    }
+
 }
 ?>
 
@@ -37,7 +54,9 @@ if($_SERVER["REQUEST_METHOD"]=="POST") {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Головна сторінка</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
 
@@ -55,8 +74,18 @@ if($_SERVER["REQUEST_METHOD"]=="POST") {
             <input type="text" class="form-control" id="price" name="price">
         </div>
         <div class="mb-3">
-            <label for="image" class="form-label">Фото</label>
-            <input type="file" class="form-control" id="image" name="image">
+            <div class="container">
+                <div class="row" id="list_images">
+
+                    <div class="col-md-3" id="selectImages">
+                        <label for="image" style="cursor: pointer;" class="form-label text-success">
+                            <i class="fa fa-plus-square-o" style="font-size:120px" aria-hidden="true"></i>
+                        </label>
+                        <input type="file" class="form-control d-none" id="image" multiple>
+                    </div>
+                </div>
+
+            </div>
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Опис</label>
@@ -68,5 +97,80 @@ if($_SERVER["REQUEST_METHOD"]=="POST") {
 </div>
 
 <script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery-3.6.2.min.js"></script>
+
+<script>
+    function uuidv4() {
+        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+    }
+    $(function (){
+        //-----------------------SELECT IMAGES LIST---------------------------
+        const image = document.getElementById("image");
+        image.onchange = function (e) {
+            const files = e.target.files;
+            for (let i = 0; i < files.length; i++) {
+                const reader = new FileReader();
+                reader.addEventListener('load', function () {
+                    const base64 = reader.result;
+                    const id = uuidv4();
+                    const data = `
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="fs-4 ms-2">
+                                    <label for="${id}">
+                                        <i class="fa fa-pencil" style="cursor: pointer;" aria-hidden="true"></i>
+                                    </label>
+                                    <input type="file" class="form-control d-none edit" id="${id}">
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="text-end fs-4 text-danger me-2 remove">
+                                    <i class="fa fa-times" style="cursor: pointer" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="${base64}" id="${id}_image" alt="photo" width="100%">
+                            <input type="hidden" id="${id}_file" value="${base64}" name="images[]">
+                        </div>
+                    `;
+                    const item = document.createElement('div');
+                    item.className = "col-md-3 item-image";
+                    item.innerHTML = data;
+                    $("#selectImages").before(item);
+                });
+                const file = files[i];
+                if (file)
+                    reader.readAsDataURL(file);
+            }
+            image.value = "";
+        }
+        //-----------------------REMOVE ITEM BY LIST---------------------------------------------
+        $("#list_images").on('click', '.remove', function () {
+            $(this).closest('.item-image').remove();
+        });
+
+        //-----------------------CHANGE IMAGE LIST ITEM-------------------------------------
+        let edit_id = 0;
+        const reader = new FileReader();
+        reader.addEventListener('load', () => {
+            const base64 = reader.result;
+            document.getElementById(`${edit_id}_image`).src = base64;
+            document.getElementById(`${edit_id}_file`).value = base64;
+        });
+
+
+        $("#list_images").on('change', '.edit', function (e) {
+            edit_id = e.target.id;
+            const file = e.target.files[0];
+            reader.readAsDataURL(file);
+            this.value = "";
+        });
+
+    });
+</script>
+
 </body>
 </html>
